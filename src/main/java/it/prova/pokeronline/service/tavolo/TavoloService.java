de3a43e8bb99193cdc94e795f6c199b67756d7fd
silvans95 <silvano.paloni@gmail.com>
2021-11-14 15:12:52 +0100

    aggiunti i service di tavolo

---
+++ src/main/java/it/prova/pokeronline/service/tavolo/TavoloService.java
@@ -0,0 +1,28 @@
+package it.prova.pokeronline.service.tavolo;
+
+
+import java.util.List;
+
+import it.prova.pokeronline.model.Tavolo;
+import it.prova.pokeronline.model.Utente;
+
+public interface TavoloService {
+	
+	public List<Tavolo> listAllTavoli() ;
+
+	public Tavolo caricaSingoloTavolo(Long id);
+	
+	public void aggiorna(Tavolo tavoloInstance);
+
+	public void inserisciNuovo(Tavolo tavoloInstance);
+
+	public void rimuovi(Tavolo tavoloInstance);
+	
+	public List<Tavolo> findByExample(Tavolo example);
+	
+	public List<Tavolo> listAllMieiTavoli(Utente user);
+	
+	public Tavolo caricaSingoloTavoloConGiocatori(Long id);
+	
+	public void rimuoviById(Long id);
+}
---
+++ src/main/java/it/prova/pokeronline/service/tavolo/TavoloServiceImpl.java
@@ -0,0 +1,64 @@
+package it.prova.pokeronline.service.tavolo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.prova.pokeronline.model.Tavolo;
+import it.prova.pokeronline.model.Utente;
+import it.prova.pokeronline.repository.tavolo.TavoloRepository;
+
+@Service
+public class TavoloServiceImpl implements TavoloService {
+
+	@Autowired
+	TavoloRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<Tavolo> listAllTavoli() {
+		return (List<Tavolo>) repository.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	public Tavolo caricaSingoloTavolo(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	@Transactional
+	public void aggiorna(Tavolo tavoloInstance) {
+		repository.save(tavoloInstance);
+	}
+
+	@Transactional
+	public void inserisciNuovo(Tavolo tavoloInstance) {
+		repository.save(tavoloInstance);
+	}
+
+	@Transactional
+	public void rimuovi(Tavolo tavoloInstance) {
+		repository.delete(tavoloInstance);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Tavolo> findByExample(Tavolo example) {
+		return repository.findByExample(example);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Tavolo> listAllMieiTavoli(Utente user) {
+		return repository.findAllByUtenteCreatore_IdIs(user.getId());
+	}
+
+	@Transactional(readOnly = true)
+	public Tavolo caricaSingoloTavoloConGiocatori(Long id) {
+		return repository.findByIdConGiocatori(id).orElse(null);
+	}
+
+	@Transactional
+	public void rimuoviById(Long id) {
+		repository.deleteById(id);
+	}
+
+}
